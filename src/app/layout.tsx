7d4ecb255fc537c0c541a242b068bf668e186c46
Michael Magan <magan@usehydra.ai>
2025-03-18 17:33:20 -0700

    simplify layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,4 @@
-import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
-
-export const metadata: Metadata = {
-  title: "CRM Dashboard",
-  description:
-    "Simple CRM application for managing leads, meetings, and messages",
-};
 
 export default function RootLayout({
   children,
@@ -26,19 +7,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      {process.env.NEXT_PUBLIC_ACKE_DOMAIN_ID && (
-        <script
-          async
-          src={`${process.env.NEXT_PUBLIC_ACKE_SERVER}/tracker.js`}
-          data-ackee-server={process.env.NEXT_PUBLIC_ACKE_SERVER}
-          data-ackee-domain-id={process.env.NEXT_PUBLIC_ACKE_DOMAIN_ID}
-        ></script>
-      )}
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <body className={`antialiased`}>{children}</body>
     </html>
   );
 }
